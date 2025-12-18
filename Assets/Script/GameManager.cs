3c9473c8d5d7e50de3e0fa664d51f6c8adac05d5
Bedirhan <bedirhan94651@gmail.com>
2025-12-18 20:59:50 +0300

    HueHoops

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    GameObject SeciliObje;
+    GameObject SeciliStand;
+    Cember _Cember;
+    public bool HareketVar;    
+    public int HedefStandSayisi;
+    int TamamlananStandSayisi;
+
+    public AudioSource[] Sesler;
+    public TextMeshProUGUI LevelAd;
+    public GameObject BolumPaneli;
+
+    private void Start()
+    {
+        LevelAd.text = "LEVEL : " + SceneManager.GetActiveScene().buildIndex;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
+            {
+                if (hit.collider != null && hit.collider.CompareTag("Stand"))
+                {
+                    // Tıklanan standı alıyoruz
+                    Stand _Stand = hit.collider.GetComponent<Stand>();
+
+                    // 1. Durum: Elimizde seçili bir halka VARSA (Yerleştirme işlemi)
+                    if (SeciliObje != null && SeciliStand != hit.collider.gameObject)
+                    {
+                        // HATA BURADAYDI: "4" yerine "_Stand.StandKapasitesi" yazdık.
+                        // Ayrıca listenin boş olup olmadığını kontrol ediyoruz.
+                        if (_Stand._Cemberler.Count < _Stand.StandKapasitesi && _Stand._Cemberler.Count != 0)
+                        {
+                            // Renkler aynı mı?
+                            if (_Cember.Renk == _Stand._Cemberler[^1].GetComponent<Cember>().Renk)
+                            {
+                                SeciliStand.GetComponent<Stand>().SoketDegistirmeIslemleri(SeciliObje);
+                                _Cember.HareketEt("PozisyonDegistir", hit.collider.gameObject, _Stand.MusaitSoketiVer(), _Stand.HareketPozisyonu);
+                                _Stand.BosOlanSoket++;
+                                _Stand._Cemberler.Add(SeciliObje);
+                                _Stand.CemberleriKontrolEt();
+
+                                // Seçimi sıfırla
+                                SeciliObje = null;
+                                SeciliStand = null;
+                            }
+                            else // Renkler farklıysa geri git
+                            {
+                                _Cember.HareketEt("SoketeGeriGit");
+                                SeciliObje = null;
+                                SeciliStand = null;
+                            }
+                        }
+                        // Stand tamamen BOŞSA (Direkt koy)
+                        else if (_Stand._Cemberler.Count == 0)
+                        {
+                            SeciliStand.GetComponent<Stand>().SoketDegistirmeIslemleri(SeciliObje);
+                            _Cember.HareketEt("PozisyonDegistir", hit.collider.gameObject, _Stand.MusaitSoketiVer(), _Stand.HareketPozisyonu);
+                            _Stand.BosOlanSoket++;
+                            _Stand._Cemberler.Add(SeciliObje);
+                            _Stand.CemberleriKontrolEt();
+
+                            SeciliObje = null;
+                            SeciliStand = null;
+                        }
+                        else // Stand DOLUYSA (Geri git)
+                        {
+                            _Cember.HareketEt("SoketeGeriGit");
+                            SeciliObje = null;
+                            SeciliStand = null;
+                        }
+                    }
+                    // Aynı standa tıkladıysak seçimi iptal et
+                    else if (SeciliStand == hit.collider.gameObject)
+                    {
+                        _Cember.HareketEt("SoketeGeriGit");
+                        SeciliObje = null;
+                        SeciliStand = null;
+                    }
+                    // 2. Durum: Elimizde seçili halka YOKSA (Seçme işlemi)
+                    else
+                    {
+                        // Standın en üstündeki objeyi almaya çalışıyoruz
+                        GameObject usttekiObje = _Stand.EnUsttekiCemberiVer();
+
+                        // EKSTRA GÜVENLİK: Eğer stand boşsa 'null' döner, kontrol etmezsek oyun çöker.
+                        if (usttekiObje != null)
+                        {
+                            SeciliObje = usttekiObje;
+                            _Cember = SeciliObje.GetComponent<Cember>();
+                            HareketVar = true;
+
+                            if (_Cember.HareketEdebilirmi)
+                            {
+                                _Cember.HareketEt("Secim", null, null, _Cember._AitOlduguStand.GetComponent<Stand>().HareketPozisyonu);
+                                SeciliStand = _Cember._AitOlduguStand;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public void StandTamamlandi()
+    {
+        TamamlananStandSayisi++;
+        if (TamamlananStandSayisi == HedefStandSayisi)
+            Kazandin(); 
+    }
+    public void SesOynat(int Index)
+    {
+        Sesler[Index].Play();
+    }
+
+    void Kazandin()
+    {
+        BolumPaneli.SetActive(true);
+        PlayerPrefs.SetInt("Level", PlayerPrefs.GetInt("Level") + 1);
+        Time.timeScale = 0;       
+    }
+
+    public void Butonlarinislemleri(string Deger)
+    {
+        switch (Deger)
+        {            
+            case "Tekrar":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                Time.timeScale = 1;
+                break;
+            case "SonrakiLevel":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                Time.timeScale = 1;
+                break;
+            case "Ayarlar":
+                // ayarlar paneli yapılabilir. sana bırakıyorum
+                break;
+
+            case "cikis":
+                Application.Quit(); // emin msin paneli ypaıalbilir. Run controlde yaptık.
+                break;
+        }
+    }
+}
---
+++ Assets/Script/Stand.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stand : MonoBehaviour
+{
+    public GameObject HareketPozisyonu;
+    public GameObject[] Soketler;
+    public int BosOlanSoket;
+    public List<GameObject> _Cemberler = new();
+    [SerializeField] private GameManager _GameManager;
+
+    int CemberTamamlanmaSayisi;
+    public int StandKapasitesi = 4;
+    public GameObject EnUsttekiCemberiVer()
+    {
+        // Eğer listede en az 1 tane çember varsa sonuncuyu ver
+        if (_Cemberler.Count > 0)
+        {
+            return _Cemberler[^1];
+        }
+        // Eğer liste boşsa 'null' (boş) döndür
+        else
+        {
+            return null;
+        }
+    }
+    public GameObject MusaitSoketiVer()
+    {
+        return Soketler[BosOlanSoket];
+    }
+    public void SoketDegistirmeIslemleri(GameObject SilinecekObje)
+    {
+        _Cemberler.Remove(SilinecekObje);
+        
+        if (_Cemberler.Count!=0)
+        {
+            BosOlanSoket--;
+            _Cemberler[^1].GetComponent<Cember>().HareketEdebilirmi = true;
+        }
+        else
+        {
+            BosOlanSoket = 0;
+        }
+    }
+    public void CemberleriKontrolEt()
+    {
+        // 1. Kapasite kontrolü (Yaptığın gibi doğru)
+        if (_Cemberler.Count == StandKapasitesi)
+        {
+            // 2. ÖNEMLİ DEĞİŞİKLİK: 
+            // Sayacı burada, yerel olarak tanımlıyoruz. 
+            // Böylece her kontrolde otomatik olarak 0'dan başlar.
+            int AyniRenkSayisi = 0;
+
+            string Renk = _Cemberler[0].GetComponent<Cember>().Renk;
+
+            foreach (var item in _Cemberler)
+            {
+                if (Renk == item.GetComponent<Cember>().Renk)
+                    AyniRenkSayisi++;
+            }
+
+            // 3. Kontrol
+            if (AyniRenkSayisi == StandKapasitesi)
+            {
+                if (_GameManager != null)
+                    _GameManager.StandTamamlandi();
+
+                TamamlanmisStandIslemleri();
+            }
+            // BURAYA DİKKAT: "else { sayac = 0 }" yazmana gerek kalmadı.
+            // Çünkü "int AyniRenkSayisi" sadece bu parantezler içinde yaşar 
+            // ve fonksiyon bitince silinir.
+        }
+    }
+    void TamamlanmisStandIslemleri()
+    {
+        foreach (var item in _Cemberler)
+        {
+            item.GetComponent<Cember>().HareketEdebilirmi = false;
+            Color32 color = item.GetComponent<MeshRenderer>().material.GetColor("_Color");
+            color.a = 150;
+            item.GetComponent<MeshRenderer>().material.SetColor("_Color",color);
+            gameObject.tag = "TamamlanmisStand";
+        }
+    }
+
+}
